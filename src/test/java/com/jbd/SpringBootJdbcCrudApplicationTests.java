5ef170f4b3d5ca20e6eb6f7a3728fcca8238b8a0
Medha Soman <medhasoman13@gmail.com>
2024-01-20 23:15:06 +0000

    project structure initial commit

---
+++ src/test/java/com/jbd/SpringBootJdbcCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jbd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJdbcCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
